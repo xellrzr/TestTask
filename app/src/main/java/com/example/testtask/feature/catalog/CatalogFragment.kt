c3e2ea6baa853c8d2ea07fe1f551b3e9203f1f94
Дмитрий <chirkov.dmitriy@adv.ru>
2023-11-05 22:18:46 +0600

    TT-001 проверка на ошибки

--- app/src/main/java/com/example/testtask/feature/catalog/CatalogFragment.kt
+++ app/src/main/java/com/example/testtask/feature/catalog/CatalogFragment.kt
@@ -1,9 +1,12 @@
 package com.example.testtask.feature.catalog
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +15,7 @@ import com.example.testtask.R
 import com.example.testtask.databinding.FragmentCatalogBinding
 import com.example.testtask.utils.catalogDiffCallback
 import com.example.testtask.utils.toFormattedString
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -36,6 +40,31 @@ class CatalogFragment : Fragment(R.layout.fragment_catalog) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvCatalog.adapter = adapter
 
+        adapter.addLoadStateListener { loadState ->
+
+            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+
+            if (loadState.append is LoadState.NotLoading && loadState.prepend is LoadState.NotLoading) {
+                val currentItems = adapter.snapshot().items
+                viewModel.updateTotalSum(currentItems)
+            }
+
+            val errorState = when {
+                loadState.mediator?.refresh is LoadState.Error -> loadState.mediator?.refresh as LoadState.Error
+                loadState.mediator?.append is LoadState.Error -> loadState.mediator?.append as LoadState.Error
+                loadState.mediator?.prepend is LoadState.Error -> loadState.mediator?.prepend as LoadState.Error
+                loadState.source.refresh is LoadState.Error -> loadState.source.refresh as LoadState.Error
+                loadState.source.append is LoadState.Error -> loadState.source.append as LoadState.Error
+                loadState.source.prepend is LoadState.Error -> loadState.source.prepend as LoadState.Error
+                else -> null
+            }
+
+            errorState?.let {
+                showSnackBarError(it.error.message ?: "")
+            }
+
+        }
+
         viewModel.catalogList.observe(viewLifecycleOwner) { pagingData ->
             lifecycleScope.launch {
                 adapter.submitData(pagingData)
@@ -46,17 +75,25 @@ class CatalogFragment : Fragment(R.layout.fragment_catalog) {
             binding.tvTotalSum.text = getString(R.string.catalog_total_sum, sum.toFormattedString())
         }
 
-        adapter.addLoadStateListener { loadState ->
-            if (loadState.append is LoadState.NotLoading && loadState.prepend is LoadState.NotLoading) {
-                val currentItems = adapter.snapshot().items
-                viewModel.updateTotalSum(currentItems)
-            }
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showSnackBarError(message: String) {
+        val errorMessage = getString(R.string.catalog_error) + message
+        Snackbar.make(binding.root,  errorMessage, Snackbar.LENGTH_INDEFINITE).apply {
+            val view = this.view
+            val params = view.layoutParams as FrameLayout.LayoutParams
+            params.gravity = Gravity.CENTER
+            view.layoutParams = params
+            setAction(getString(R.string.catalog_repeat)) {
+                adapter.retry()
+            }
+            show()
+        }
     }
 
 }
